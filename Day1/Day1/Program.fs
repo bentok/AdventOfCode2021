4de200d3b3960ae9d24b70b932b610da46b7c279
bentok <bscopeland@gmail.com>
2021-12-02 14:08:55 -0500

    Day 1 excercise 2

--- Day1/Day1/Program.fs
+++ Day1/Day1/Program.fs
@@ -12,10 +12,25 @@ module Program =
     let accumulate (accumulator, previous) current =
         gt accumulator (current, previous)
         |> inc
+        
+    let partition i =
+        if i < Array.length input - 2
+        then [input.[i]; input.[i + 1]; input.[i + 2];]
+        else []
+        
+    let part2 =
+        let indexedList = [0..Array.length input - 1]
+        
+        indexedList
+        |> List.map partition
+        |> List.filter (fun l -> not (List.isEmpty l))
+        |> List.map List.sum
+        |> List.toArray
 
     [<EntryPoint>]
     let main args =
-        Array.fold accumulate (0, 0) input
+        // part1 is the same but: Array.fold accumulate (0, 0) input
+        Array.fold accumulate (0, 0) part2
         |> sprintf "%A"
         |> ignore
 
